e70805738641009fdcd7e9764c1b881ba173cd74
Ulf FrostyPaw <19324157@uagro.mx>
2023-01-27 22:11:03 -0600

    Bug: Revisar /contact en el componente de Icons

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,12 @@
 export default {
   logo: <span>Ulf FrostyPaw</span>,
   project: {
-    link: 'https://github.com/shuding/nextra',
+    link: 'https://github.com/ulf-frostypaw/portfolio',
   },
+  footer: {
+    text: <span>
+      MIT {new Date().getFullYear()} © Ofrecido por <a href="https://nextra.site" target="_blank">Nextra</a>. Creado por <a href="/contact">Ulf FrostyPaw</a>
+    </span>
+  }
   // ...
 }
